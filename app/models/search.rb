dcbc65ff78d03d2269d2eca649e45782acd5b61d
Igor Fernandes <igorrfc10@gmail.com>
2017-09-09 17:11:00 -0300

    Update the Search model
    creating a new scope called 'by_popularity'.

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,4 +1,6 @@
 # Search - represents the searches made for application's resources
 class Search < ApplicationRecord
   has_many :locations
+
+  scope :by_popularity, -> { order(popularity: :desc) }
 end
