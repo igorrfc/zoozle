5effc64e7f3e18549c8a3ef06752f750bae1a391
Igor Fernandes <igorrfc10@gmail.com>
2017-09-09 17:13:22 -0300

    Create the ArticlesController
    with a index action which is used as endpoint on the main's search form.

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,18 @@
+class ArticlesController < ApplicationController
+  def index
+    if search_params[:description].present?
+      Searches::Processor.call(
+        search_params[:description],
+        remote_ip: request.remote_ip
+      )
+    end
+
+    head :ok
+  end
+
+  private
+
+  def search_params
+    params.require(:search).permit(:description)
+  end
+end
