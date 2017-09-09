0acf7467b07b06e26ee069f9eb204add8f35d0d4
Igor Fernandes <igorrfc10@gmail.com>
2017-09-09 09:03:03 -0300

    Update the config/application file
    preloading the app/middlewares path.

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,7 @@ module Zoozle
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.autoload_paths << "#{Rails.root}/app/queries"
+    config.autoload_paths << Rails.root.join('app', 'queries')
+    config.autoload_paths << Rails.root.join('app', 'middlewares')
   end
 end
