bc7e48303d20a98b4379fa05bee7063c02ad630d
Igor Fernandes <igorrfc10@gmail.com>
2017-09-09 17:12:14 -0300

    Update the SearchesController index action
    to fetch all the Searches ordering them by their popularity. Changes:
    
      - Created the index view to show all the most popular searches;

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,18 +1,5 @@
 class SearchesController < ApplicationController
   def index
-    if search_params[:description].present?
-      Searches::Processor.call(
-        search_params[:description],
-        remote_ip: request.remote_ip
-      )
-    end
-
-    head :ok
-  end
-
-  private
-
-  def search_params
-    params.require(:search).permit(:description)
+    @searches = Search.by_popularity
   end
 end
