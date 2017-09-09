4477d571432c163b4d711dcfdaf8728cca19ded9
Igor Fernandes <igorrfc10@gmail.com>
2017-09-09 17:56:23 -0300

    Create the sidekiq initializer

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,21 @@
+require 'sidekiq/api'
+
+Sidekiq.configure_server do |config|
+  config.redis = { url: ENV["REDIS_URL"] || "redis://localhost:6379/" }
+
+  config.server_middleware do |chain|
+    chain.add(Sidekiq::Status::ServerMiddleware, expiration: 30.minutes)
+  end
+
+  config.client_middleware do |chain|
+    chain.add(Sidekiq::Status::ClientMiddleware, expiration: 30.minutes)
+  end
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: ENV["REDIS_URL"] || "redis://localhost:6379/" }
+
+  config.client_middleware do |chain|
+    chain.add(Sidekiq::Status::ClientMiddleware, expiration: 30.minutes)
+  end
+end
